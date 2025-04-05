8ae712a6451f878f4f59e581c9f6c514e84a778a
Alvin21Bon <alvin21bon@gmail.com>
2025-04-05 10:33:33 -0400

    set tabstop to 2 for lua files in nvim config

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -12,6 +12,22 @@ return {
 			},
 		},
 
+		autocmds = {
+			filetype_tabstops = {
+				{
+					event = "FileType",
+					pattern = "lua",
+					desc = "Set Lua tabstop only for nvim config",
+					callback = function(event)
+						local buf_path = vim.api.nvim_buf_get_name(event.buf)
+						local config_path = vim.fn.stdpath("config") .. "/"
+
+						if buf_path:match("^" .. config_path) then vim.opt_local.tabstop = 2 end
+					end,
+				},
+			},
+		},
+
 		mappings = {
 			n = {
 				["<C-d>"] = {"<C-d>zz"},
