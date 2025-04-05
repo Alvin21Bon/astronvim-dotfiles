776a67a4d22eb2638ee53005cd901d0bfe0d9138
Alvin21Bon <alvin21bon@gmail.com>
2025-04-05 08:54:07 -0400

    do not expand tab and tab width to 8

---
+++ lua/plugins/astrocore.lua
@@ -0,0 +1,15 @@
+---@type LazySpec
+return {
+	"AstroNvim/astrocore",
+
+	---@type AstroCoreOpts
+	opts = {
+		options = {
+			opt = {
+				expandtab = false,
+				tabstop = 8,
+				shiftwidth = 0,
+			},
+		},
+	},
+}
